6819f8a6561b7fba055aea40109a229388531a92
Sergio González <bigmonachus@gmail.com>
2010-10-26 22:15:59 -0500

    This error seems to detect edges..

--- blur.cc
+++ blur.cc
@@ -17,9 +17,6 @@ int main(int argc, char *argv[])
   unsigned char* texels = img.data();
   unsigned char* resulttexels = new unsigned char [size];
 
-  resulttexels = new unsigned char [size];
-  
-  
   //Create buffer
   
   //source image texels
@@ -50,13 +47,14 @@ int main(int argc, char *argv[])
 				texels,
 				0,NULL,NULL);
   CL_CHK (clerr);
-
+  
   
   cl_program prgrm = ocl.createProgram("blur.cl");
   ocl.buildProgram (prgrm);
   cl_kernel kernel = ocl.createKernel (prgrm,"blur");
+  CL_CHK (clerr);
   clerr = clSetKernelArg (kernel,0,sizeof (unsigned int),&width);
-  clerr = clSetKernelArg (kernel,0,sizeof (unsigned int),&height);
+  clerr = clSetKernelArg (kernel,1,sizeof (unsigned int),&height);
   clerr = clSetKernelArg (kernel,2,sizeof (cl_mem),(void*)&tex_buffer);
   CL_CHK (clerr);
   clerr = clSetKernelArg (kernel,3,sizeof (cl_mem),(void*)&res_buffer);
@@ -64,25 +62,28 @@ int main(int argc, char *argv[])
 
   
 
-  size_t global_wsize = size;
+  size_t global_wsize = width*height;
   size_t local_wsize = 32*4;
+
   
+    
   clerr = clEnqueueNDRangeKernel (q,kernel,
 				  1,//work dim
 				  NULL,//offset
 				  &global_wsize,
 				  &local_wsize,
 				  0,NULL,NULL);
-  
+    
   clEnqueueReadBuffer (q,
-			res_buffer,
-			CL_TRUE,0,
-			size,
+		       res_buffer,
+		       CL_TRUE,0,
+		       size,
 		       res.data(),
 		       0,NULL,NULL);
 
   
   res.display ("result");
+  res.save ("blured.tga");
   delete [] resulttexels;
   return 0;
 }
